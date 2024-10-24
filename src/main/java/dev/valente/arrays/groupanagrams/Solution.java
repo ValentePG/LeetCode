d242394096c4b154d8d747e6b4713862dfd7cb2f
Gabriel Valente <112831085+ValentePG@users.noreply.github.com>
2024-10-24 17:26:30 -0300

    groupAnagrams

---
+++ src/main/java/dev/valente/arrays/groupanagrams/Solution.java
@@ -0,0 +1,33 @@
+package dev.valente.arrays.groupanagrams;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+public class Solution {
+    public static void main(String[] args) {
+        String[] strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+        System.out.println(groupAnagrams(strs));
+    }
+
+    public static List<List<String>> groupAnagrams(String[] strs){
+        // Checar tabela ASCII
+
+        HashMap<String, List<String>> res = new HashMap<>();
+        for(String s : strs){
+            int[] count = new int[26];
+            for(char c : s.toCharArray()){
+                count[c - 'a']++;
+            }
+            String key = Arrays.toString(count);
+            res.putIfAbsent(key, new ArrayList<>());
+            res.get(key).add(s);
+
+        }
+        return new ArrayList<>(res.values());
+
+    }
+
+
+}
